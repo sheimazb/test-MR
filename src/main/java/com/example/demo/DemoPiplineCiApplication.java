74f3b3cffd680157eabfab550f05156a5e42e762
chaima zbidi <zbedichaima@gmail.com>
2025-06-28 16:53:28 +0200

    Fix configuration without secrets

--- src/main/java/com/example/demo/DemoPiplineCiApplication.java
+++ src/main/java/com/example/demo/DemoPiplineCiApplication.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoPiplineCiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoPiplineCiApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoPiplineCiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		
 		System.out.println("✅ Ticket Solution Pipeline Application Started");
+		System.out.println("✅ Running with profile: " + String.join(", ", env.getActiveProfiles()));
+		System.out.println("✅ Access URL: http://localhost:" + env.getProperty("server.port") + "/static/index.html");
 	}
 
 }

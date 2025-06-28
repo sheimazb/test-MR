d64381e44652fde62d24c5267a1b650f987a885f
chaima zbidi <zbedichaima@gmail.com>
2025-06-29 00:06:59 +0200

    Add solution for ticket #1: Solution for test ticket

---
+++ src/main/java/solutions/Ticket1Solution.java
@@ -0,0 +1,14 @@
+package solutions;
+
+/**
+ * Solution for Ticket #1
+ * Description: Solution for test ticket
+ * Generated on: 2025-06-29T00:06:59.808293200
+ */
+public class Ticket1Solution {
+
+    /**
+     * Solution for test ticket
+     */
+    public void solveProblem() { System.out.println("Problem solved!"); }
+}
